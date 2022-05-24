f54188daed0016ca23d24fe01df2160bacfe5e70
Gabriel Almeida <91497255+GabrielAlmeidaGOK@users.noreply.github.com>
2022-05-24 15:17:01 -0300

    socket.io-example-chat

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const count = io.engine.clientsCount;
+
+    usernames = [];
+server.listen(process.env.PORT || 3000);
+
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/js', express.static(__dirname + '/js'));
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+  //res.sendFile(__dirname + '/style.css');
+});
+
+io.sockets.on('connection', function(socket){
+  
+  socket.on('new user', function(data, callback){
+    console.log(client.broadcast({count: Object.keys(clients).length}))
+    if(usernames.indexOf(data) != -1){
+      callback(false);
+    }
+    else{
+      callback(true);
+      socket.username = data;
+      usernames.push(socket.username);
+      updateUsernames();
+    }
+  });
+  
+  function updateUsernames(){
+    io.sockets.emit('usernames', usernames);
+  }
+  
+  socket.on('send message', function(data){
+    io.sockets.emit('new message', {msg: data, user: socket.username});
+  });
+  
+  socket.on('disconnect', function(data){
+    if(!socket.username) return;
+    usernames.splice(usernames.indexOf(socket.username, 1));
+    updateUsernames();
+  });
+});
